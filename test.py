4a726fa8550acdbfc610455319deb1ee002fad4e
Ben Leung <benleung@umich.edu>
2018-10-07 20:59:13 -0700

    updates weekend

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-test = str()
-print(type(test))
+l = ['colorado','utah','wyoming','montana','idaho','new-mexico','arizona','california','lake-tahoe','nevada','oregon','washington','alaska','new-york','pennsylvania','vermont','new-hampshire','maine','massachusetts','michigan','minnesota','wisconsin','alberta','british-columbia','ontario','quebec']
+for state in l:
+	print(state)
