2b12e72a9884e920b6329bd7b8f313432453ceaf
Ben Leung <benleung@umich.edu>
2018-10-15 17:12:16 -0700

    cleaning repo for submission

--- test.py
+++
@@ -1,3 +0,0 @@
-l = ['colorado','utah','wyoming','montana','idaho','new-mexico','arizona','california','lake-tahoe','nevada','oregon','washington','alaska','new-york','pennsylvania','vermont','new-hampshire','maine','massachusetts','michigan','minnesota','wisconsin','alberta','british-columbia','ontario','quebec']
-for state in l:
-	print(state)
